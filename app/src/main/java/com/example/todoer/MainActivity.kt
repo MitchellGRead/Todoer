f39c65ed4f75cb3f4535c32125b03761380750bb
MitchellGRead <mitchell.g.read@gmail.com>
2020-10-25 16:30:10 -0700

    Added in navigate up functionality

--- app/src/main/java/com/example/todoer/MainActivity.kt
+++ app/src/main/java/com/example/todoer/MainActivity.kt
@@ -2,12 +2,25 @@ package com.example.todoer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = this.findNavController(R.id.navHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
     }
 }
