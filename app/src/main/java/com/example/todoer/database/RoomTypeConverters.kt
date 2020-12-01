8aff0d781f0cc1a03123143a3929310bbe6735e1
MitchellGRead <mitchell.g.read@gmail.com>
2020-12-01 07:58:39 -0800

    Update the database to now be taking the ListType

--- app/src/main/java/com/example/todoer/database/RoomTypeConverters.kt
+++ app/src/main/java/com/example/todoer/database/RoomTypeConverters.kt
@@ -1,6 +1,9 @@
 package com.example.todoer.database
 
 import androidx.room.TypeConverter
+import com.example.todoer.ui.createlist.CheckList
+import com.example.todoer.ui.createlist.ListType
+import com.example.todoer.ui.createlist.Note
 import java.util.*
 
 class RoomTypeConverters {
@@ -10,4 +13,17 @@ class RoomTypeConverters {
 
     @TypeConverter
     fun dateToTimestamp(date: Date): Long = date.time
+
+    @TypeConverter
+    fun fromListType(value: String): ListType {
+        return ListType.toListType(value)
+    }
+
+    @TypeConverter
+    fun listTypeToString(listType: ListType): String {
+        return when (listType) {
+            is CheckList -> listType.value
+            is Note -> listType.value
+        }
+    }
 }
--- app/src/main/java/com/example/todoer/database/models/TodoList.kt
+++ app/src/main/java/com/example/todoer/database/models/TodoList.kt
@@ -3,6 +3,7 @@ package com.example.todoer.database.models
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.example.todoer.ui.createlist.ListType
 import java.util.*
 
 @Entity(tableName = "todo_list_table")
@@ -18,7 +19,7 @@ data class TodoList(
     val listName: String = "",
 
     @ColumnInfo(name = "list_type")
-    val listType: String = "",
+    val listType: ListType,
 
     @ColumnInfo(name = "completed_tasks")
     val completedTasks: Int = 0,
