3940c4f99d8c413ba59ece16da60d2595b4fe5ab
MitchellGRead <mitchell.g.read@gmail.com>
2021-01-16 18:59:57 -0800

    update viewmodel to add sharing the notes description

--- app/src/main/java/com/example/todoer/ui/notedetails/NoteDetailsViewModel.kt
+++ app/src/main/java/com/example/todoer/ui/notedetails/NoteDetailsViewModel.kt
@@ -3,6 +3,7 @@ package com.example.todoer.ui.notedetails
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
+import com.example.todoer.base.BaseViewModel
 import com.example.todoer.domain.TodoNoteRepo
 import com.squareup.inject.assisted.Assisted
 import com.squareup.inject.assisted.AssistedInject
@@ -13,10 +14,17 @@ import timber.log.Timber
 class NoteDetailsViewModel @AssistedInject constructor(
     @Assisted private val noteId: Long,
     private val noteRepo: TodoNoteRepo
-) : ViewModel() {
+) : BaseViewModel<NoteAction>() {
 
     init {
         Timber.d("NoteDetailsViewModel created")
+    }
+
+    fun shareTodo() {
+        viewModelScope.launch {
+            val description = getNoteDescription()
+            setAction(ShareNote(description))
+        }
     }
 
     suspend fun getNoteDescription(): String {
